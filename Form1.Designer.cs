dc45fd8ea9ea08226c0b3af02c69122a62282c97
JAVED IQAL <engrjavediqbal837@yahoo.com>
2015-05-12 16:32:09 +0500

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace DownloadManager2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.prgDownload = new System.Windows.Forms.ProgressBar();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.btnPauseResume = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Location = new System.Drawing.Point(12, 9);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(83, 13);
+            this.lblUrl.TabIndex = 0;
+            this.lblUrl.Text = "Download URL:";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(101, 6);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(347, 20);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "http://download.microsoft.com/download/1/8/2/182200e0-cf87-47d6-87a7-32cb36bbfa42" +
+    "/AprilFool_Habib_and_Anson.wmv";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(193, 58);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(93, 23);
+            this.btnDownload.TabIndex = 2;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // prgDownload
+            // 
+            this.prgDownload.Location = new System.Drawing.Point(12, 118);
+            this.prgDownload.Name = "prgDownload";
+            this.prgDownload.Size = new System.Drawing.Size(436, 23);
+            this.prgDownload.TabIndex = 3;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(101, 32);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(347, 20);
+            this.txtPath.TabIndex = 4;
+            this.txtPath.Text = "C:\\Filename.wmv";
+            this.txtPath.TextChanged += new System.EventHandler(this.txtPath_TextChanged);
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(12, 35);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(83, 13);
+            this.lblPath.TabIndex = 5;
+            this.lblPath.Text = "Download Path:";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(373, 58);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProgress.Location = new System.Drawing.Point(8, 95);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(151, 20);
+            this.lblProgress.TabIndex = 8;
+            this.lblProgress.Text = "Awaiting Download";
+            // 
+            // btnPauseResume
+            // 
+            this.btnPauseResume.Enabled = false;
+            this.btnPauseResume.Location = new System.Drawing.Point(292, 58);
+            this.btnPauseResume.Name = "btnPauseResume";
+            this.btnPauseResume.Size = new System.Drawing.Size(75, 23);
+            this.btnPauseResume.TabIndex = 9;
+            this.btnPauseResume.Text = "Pause";
+            this.btnPauseResume.UseVisualStyleBackColor = true;
+            this.btnPauseResume.Click += new System.EventHandler(this.btnPauseResume_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 158);
+            this.Controls.Add(this.btnPauseResume);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.prgDownload);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.lblUrl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Download Manager by Javed";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUrl;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.ProgressBar prgDownload;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.Button btnPauseResume;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace DownloadManager2
+{
+    public partial class Form1 : Form
+    {
+
+        // Definitions
+        // The thread inside which the download happens
+        private Thread thrDownload;
+        // The stream of data retrieved from the web server
+        private Stream strResponse;
+        // The stream of data that we write to the harddrive
+        private Stream strLocal;
+        // The request to the web server for file information
+        private HttpWebRequest webRequest;
+        // The response from the web server containing information about the file
+        private HttpWebResponse webResponse;
+        // The progress of the download in percentage
+        private static int PercentProgress;
+        // The delegate which we will call from the thread to update the form
+        private delegate void UpdateProgessCallback(Int64 BytesRead, Int64 TotalBytes);
+        // When to pause
+        bool goPause = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (thrDownload != null && thrDownload.ThreadState == ThreadState.Running)
+            {
+                MessageBox.Show("A download is already running. Please either the stop the current download or await for its completion before starting a new one.", "Download in progress", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                // Let the user know we are connecting to the server
+                lblProgress.Text = "Download Starting";
+                // Create a new thread that calls the Download() method
+                thrDownload = new Thread(new ParameterizedThreadStart(Download));
+                // Start the thread, and thus call Download(); start downloading from the beginning (0)
+                thrDownload.Start(0);
+                // Enable the Pause/Resume button
+                btnPauseResume.Enabled = true;
+                // Set the button's caption to Pause because a download is in progress
+                btnPauseResume.Text = "Pause";
+            }
+        }
+
+        private void UpdateProgress(Int64 BytesRead, Int64 TotalBytes)
+        {
+            // Calculate the download progress in percentages
+            PercentProgress = Convert.ToInt32((BytesRead * 100) / TotalBytes);
+            // Make progress on the progress bar
+            prgDownload.Value = PercentProgress;
+            // Display the current progress on the form
+            //lblProgress.Text = "Downloaded "  + PercentProgress + "%";
+            lblProgress.Text = "Downloaded " + BytesRead + " out of " + TotalBytes + " (" + PercentProgress + "%)";
+        }
+
+        private void Download(object startPoint)
+        {
+            try
+            {
+                // Put the object argument into an int variable
+                int startPointInt = Convert.ToInt32(startPoint);
+                // Create a request to the file we are downloading
+                webRequest = (HttpWebRequest)WebRequest.Create(txtUrl.Text);
+                // Set the starting point of the request
+                webRequest.AddRange(startPointInt);
+
+                // Set default authentication for retrieving the file
+                webRequest.Credentials = CredentialCache.DefaultCredentials;
+                // Retrieve the response from the server
+                webResponse = (HttpWebResponse)webRequest.GetResponse();
+                // Ask the server for the file size and store it
+                Int64 fileSize = webResponse.ContentLength;
+                string content = webResponse.ContentType;;
+                // Open the URL for download 
+                strResponse = webResponse.GetResponseStream();
+
+                // Create a new file stream where we will be saving the data (local drive)
+                if (startPointInt == 0)
+                {
+                    strLocal = new FileStream(txtPath.Text, FileMode.Create, FileAccess.Write, FileShare.None);
+                }
+                else
+                {
+                    strLocal = new FileStream(txtPath.Text, FileMode.Append, FileAccess.Write, FileShare.None);
+                }
+                
+                // It will store the current number of bytes we retrieved from the server
+                int bytesSize = 0;
+                // A buffer for storing and writing the data retrieved from the server
+                byte[] downBuffer = new byte[2048];
+
+                // Loop through the buffer until the buffer is empty
+                while ((bytesSize = strResponse.Read(downBuffer, 0, downBuffer.Length)) > 0)
+                {
+                    // Write the data from the buffer to the local hard drive
+                    strLocal.Write(downBuffer, 0, bytesSize);
+                    // Invoke the method that updates the form's label and progress bar
+                    this.Invoke(new UpdateProgessCallback(this.UpdateProgress), new object[] { strLocal.Length, fileSize + startPointInt });
+
+                    if (goPause == true)
+                    {
+                        break;
+                    }
+                }
+            }
+            finally
+            {
+                // When the above code has ended, close the streams
+                strResponse.Close();
+                strLocal.Close();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            // Abort the thread that's downloading
+            thrDownload.Abort();
+            // Close the web response and the streams
+            webResponse.Close();
+            strResponse.Close();
+            strLocal.Close();
+            // Set the progress bar back to 0 and the label
+            prgDownload.Value = 0;
+            lblProgress.Text = "Download Stopped";
+            // Disable the Pause/Resume button because the download has ended
+            btnPauseResume.Enabled = false;
+        }
+
+        private void btnPauseResume_Click(object sender, EventArgs e)
+        {
+            // If the thread exists
+            if (thrDownload != null)
+            {
+                if (btnPauseResume.Text == "Pause")
+                {
+                    // The Pause/Resume button was set to Pause, thus pause the download
+                    goPause = true;
+
+                    // Now that the download was paused, turn the button into a resume button
+                    btnPauseResume.Text = "Resume";
+
+                    // Close the web response and the streams
+                    webResponse.Close();
+                    strResponse.Close();
+                    strLocal.Close();
+                    // Abort the thread that's downloading
+                    thrDownload.Abort();
+                }
+                else
+                {
+                    // The Pause/Resume button was set to Resume, thus resume the download
+                    goPause = false;
+
+                    // Now that the download was resumed, turn the button into a pause button
+                    btnPauseResume.Text = "Pause";
+
+                    long startPoint = 0;
+
+                    if (File.Exists(txtPath.Text))
+                    {
+                        startPoint = new FileInfo(txtPath.Text).Length;
+                    }
+                    else
+                    {
+                        MessageBox.Show("The file you choosed to resume doesn't exist.", "Could not resume", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                    // Let the user know we are connecting to the server
+                    lblProgress.Text = "Download Resuming";
+                    // Create a new thread that calls the Download() method
+                    thrDownload = new Thread(new ParameterizedThreadStart(Download));
+                    // Start the thread, and thus call Download()
+                    thrDownload.Start(startPoint);
+                    // Enable the Pause/Resume button
+                    btnPauseResume.Enabled = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("A download does not appear to be in progress.", "Could not pause", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
